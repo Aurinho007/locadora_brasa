f96c30b24d26c362018c2d4a8ad09a6cadebfc79
F3868209 Guilherme Rodrigues Lodron Pires <guilherme.pires@bb.com.br>
2022-07-13 17:23:47 -0300

    Adiciona logica da 'api'

---
+++ js/index.js
@@ -0,0 +1,19 @@
+import { Controller } from "./controller.js";
+
+
+// testando controller
+const controller = new Controller()
+// controller.buscaFilmes()
+// // controller.buscaPorID(2)
+// const botao = document.querySelector('.adiciona')
+// botao.addEventListener('click', (e) => {
+//     controller.adicionaFilme({
+//         "nome": "Frozen",
+//         "categoria": "Aventura",
+//         "classificacao": "Livre",
+//         "duracao": "1h:40min",
+//         "descricao": "Uma nova aventura na neve",
+//         "cartaz": "",
+//     })
+    
+// })
