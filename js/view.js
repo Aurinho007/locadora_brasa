34ba9878466e7038cc3628a088db105d02babee9
= <carlosedsb0110@gmail.com>
2022-07-14 23:29:26 -0300

    Adiciona alguns filmes

--- js/view.js
+++ js/view.js
@@ -43,11 +43,11 @@ function populaView(filmesResponse) {
             <span>${categoria}</span>
 
             <div class="carrossel-categoria">
-                ${filmesFiltrados.length > 3 ? '<div class="seta-esquerda"><</div>' : ''}
+                ${filmesFiltrados.length > 3 ? '<div class="seta-esquerda"><svg xmlns="http://www.w3.org/2000/svg"  width="50" height="50" style="fill: rgba(255, 214, 0, 1)" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z"/></div>' : ''}
 
                 <div class="filmes">${filmesHTML}</div>
 
-                ${filmesFiltrados.length > 3 ? '<div class="seta-direita">></div>' : 'maaaaaaaaaaaaaaaaoi'}
+                ${filmesFiltrados.length > 3 ? '<div class="seta-direita"><svg xmlns="http://www.w3.org/2000/svg"  width="50" height="50" style="fill: rgba(255, 214, 0, 1)" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M96 480c-8.188 0-16.38-3.125-22.62-9.375c-12.5-12.5-12.5-32.75 0-45.25L242.8 256L73.38 86.63c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0l192 192c12.5 12.5 12.5 32.75 0 45.25l-192 192C112.4 476.9 104.2 480 96 480z"/></svg></div>' : ''}
             </div>
         </div>
         `
